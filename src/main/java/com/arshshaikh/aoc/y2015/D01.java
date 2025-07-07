3124e900f32ebe357e3d0705142149605e884cb6
Arsh Shaikh <arsh.js.dev@gmail.com>
2025-07-07 21:28:11 +0100

    :art: tests updated to read from file

--- src/main/java/com/arshshaikh/aoc/y2015/D01.java
+++ src/main/java/com/arshshaikh/aoc/y2015/D01.java
@@ -1,12 +1,12 @@
 package com.arshshaikh.aoc.y2015;
 
 public class D01 {
-    public static int solutionPartOne(String instructions) {
+    static int solutionPartOne(String instructions) {
         final int up = '(';
         return instructions.chars().map(f -> f == up ? 1 : -1).reduce(0, Integer::sum);
     }
 
-    public static int solutionPartTwo(String instructions) {
+    static int solutionPartTwo(String instructions) {
         int floor = 0;
         for (int i = 0; i < instructions.length(); i++) {
             floor += instructions.charAt(i) == '(' ? 1 : -1;
