69aeec299363a4454c3a91bd0a09c0b25ddff5d6
Arsh Shaikh <arsh.js.dev@gmail.com>
2025-07-07 20:33:19 +0100

    :sparkles: y2015 day01 puzzle02 added

--- src/main/java/com/arshshaikh/aoc/y2015/D01.java
+++ src/main/java/com/arshshaikh/aoc/y2015/D01.java
@@ -1,8 +1,21 @@
 package com.arshshaikh.aoc.y2015;
 
 public class D01 {
-    public static int solution(String instructions) {
+    public static int solutionPartOne(String instructions) {
         final int up = '(';
         return instructions.chars().map(f -> f == up ? 1 : -1).reduce(0, Integer::sum);
+    }
+
+    public static int solutionPartTwo(String instructions) {
+        int floor = 0;
+        for (int i = 0; i < instructions.length(); i++) {
+            floor += instructions.charAt(i) == '(' ? 1 : -1;
+
+            if (floor == -1) {
+                return i + 1;
+            }
+        }
+
+        return 0;
     }
 }
